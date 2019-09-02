5c81011675d3a19fac38fb2dd44d794b8335cf54
Phoenix1747 <apertured@public-files.de>
2019-09-02 14:03:51 +0200

    add pendulum weight

--- pendulum.gd
+++ pendulum.gd
@@ -8,17 +8,24 @@ var l = 2 #length of the pendulum; 1/100 of the pendulum graphic
 var g = 9.81 #m/s^2, gravitational acceleration
 var gamma = 0.1 #damping ratio to take friction into account
 
+export(Color) var weight_color = Color(1,1,1,1)
+var new_vector = Vector2()
+
 # INIT FUNCTION
 func _ready():
 	updatePhi() #initial set correct angle
+
+# DRAW THE PENDULUM WEIGHTED TIP
+func _draw():
+	draw_circle(new_vector, 10, weight_color)
 
 # FUNCTION TO UPDATE PHI
 func updatePhi(a=phi):
 	var offset = 5*PI/2 #offset factor to correct angles
 	var graphRel = 100 #The actual pendulum graph will be bigger by this factor than L
 	
-	var newVector = Vector2(l * cos(a + offset) * graphRel, l * sin(a + offset) * graphRel)
-	set_point_position(1, newVector)
+	new_vector = Vector2(l * cos(a + offset) * graphRel, l * sin(a + offset) * graphRel)
+	set_point_position(1, new_vector)
 
 # COMPUTE SECOND DERIVATIVE OF PHI BASED ON THE DIFFERENTIAL EQUATION
 func phiSecond(a, ao):
@@ -30,3 +37,4 @@ func _physics_process(delta):
 	phi_first += phiSecond(phi, phi_first) * delta #phi_second * delta
 	
 	updatePhi()
+	update()
