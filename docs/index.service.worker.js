964ab1199a64b5974f1002faa704d0b391549d5c
NuclearPhoenixx <nuclearphoenix@public-files.de>
2023-11-02 14:09:44 +0100

    update Godot 3.5.3

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1659947448|20136822";
+const CACHE_VERSION = "1698930521|31736458";
 const CACHE_PREFIX = "2D Pendulum Simu-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
